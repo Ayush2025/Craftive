a5273ab528f743b024fc475e3ca186557c94f58a
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 20:40:32 +0530

    Update start-tmux-back.sh
    Signed-off-by: Ayush2025 <88815689+Ayush2025@users.noreply.github.com>

--- docker/devenv/files/start-tmux-back.sh
+++ docker/devenv/files/start-tmux-back.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chown penpot:users /home/penpot
+sudo chown craftive:users /home/craftive
 
 cd ~;
 
@@ -9,25 +9,25 @@ source ~/.bashrc
 set -e;
 
 echo "[start-tmux.sh] Installing node dependencies"
-pushd ~/penpot/exporter/
+pushd ~/craftive/exporter/
 yarn install
 popd
 
-tmux -2 new-session -d -s penpot
+tmux -2 new-session -d -s craftive
 
-tmux rename-window -t penpot:0 'exporter'
-tmux select-window -t penpot:0
-tmux send-keys -t penpot 'cd penpot/exporter' enter C-l
-tmux send-keys -t penpot 'rm -f target/app.js*' enter C-l
-tmux send-keys -t penpot 'clojure -M:dev:shadow-cljs watch main' enter
+tmux rename-window -t craftive:0 'exporter'
+tmux select-window -t craftive:0
+tmux send-keys -t craftive 'cd craftive/exporter' enter C-l
+tmux send-keys -t craftive 'rm -f target/app.js*' enter C-l
+tmux send-keys -t craftive 'clojure -M:dev:shadow-cljs watch main' enter
 
 tmux split-window -v
-tmux send-keys -t penpot 'cd penpot/exporter' enter C-l
-tmux send-keys -t penpot './scripts/wait-and-start.sh' enter
+tmux send-keys -t craftive 'cd craftive/exporter' enter C-l
+tmux send-keys -t craftive './scripts/wait-and-start.sh' enter
 
-tmux new-window -t penpot:1 -n 'backend'
-tmux select-window -t penpot:1
-tmux send-keys -t penpot 'cd penpot/backend' enter C-l
-tmux send-keys -t penpot './scripts/start-dev' enter
+tmux new-window -t craftive:1 -n 'backend'
+tmux select-window -t craftive:1
+tmux send-keys -t craftive 'cd craftive/backend' enter C-l
+tmux send-keys -t craftive './scripts/start-dev' enter
 
-tmux -2 attach-session -t penpot
+tmux -2 attach-session -t craftive
