7f12ec2b3dd99c60bade140ab75599fdbd31edac
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 20:45:12 +0530

    Update nginx-entrypoint.sh
    Signed-off-by: Ayush2025 <88815689+Ayush2025@users.noreply.github.com>

--- docker/images/files/nginx-entrypoint.sh
+++ docker/images/files/nginx-entrypoint.sh
@@ -4,9 +4,9 @@
 ## Air Gapped config
 #########################################
 
-if [[ $PENPOT_FLAGS == *"enable-air-gapped-conf"* ]]; then
+if [[ $CRAFTIVE_FLAGS == *"enable-air-gapped-conf"* ]]; then
     export INCLUDE_PROXIES=""
-    export PENPOT_FLAGS="$PENPOT_FLAGS disable-google-fonts-provider disable-dashboard-templates-section"
+    export CRAFTIVE_FLAGS="$CRAFTIVE_FLAGS disable-google-fonts-provider disable-dashboard-templates-section"
 else
     export INCLUDE_PROXIES="include /etc/nginx/nginx-proxies.conf;"
 fi
@@ -16,9 +16,9 @@ fi
 #########################################
 
 update_flags() {
-  if [ -n "$PENPOT_FLAGS" ]; then
+  if [ -n "$CRAFTIVE_FLAGS" ]; then
     sed -i \
-      -e "s|^//var penpotFlags = .*;|var penpotFlags = \"$PENPOT_FLAGS\";|g" \
+      -e "s|^//var craftiveFlags = .*;|var craftiveFlags = \"$CRAFTIVE_FLAGS\";|g" \
       "$1"
   fi
 }
@@ -31,16 +31,16 @@ update_flags /var/www/app/js/config.js
 ## Nginx Config
 #########################################
 
-export PENPOT_BACKEND_URI=${PENPOT_BACKEND_URI:-http://penpot-backend:6060}
-export PENPOT_EXPORTER_URI=${PENPOT_EXPORTER_URI:-http://penpot-exporter:6061}
-PENPOT_DEFAULT_INTERNAL_RESOLVER="$(awk 'BEGIN{ORS=" "} $1=="nameserver" { sub(/%.*$/,"",$2); print ($2 ~ ":")? "["$2"]": $2}' /etc/resolv.conf)"
-export PENPOT_INTERNAL_RESOLVER=${PENPOT_INTERNAL_RESOLVER:-$PENPOT_DEFAULT_INTERNAL_RESOLVER}
-export PENPOT_HTTP_SERVER_MAX_MULTIPART_BODY_SIZE=${PENPOT_HTTP_SERVER_MAX_MULTIPART_BODY_SIZE:-367001600} # Default to 350MiB
+export CRAFTIVE_BACKEND_URI=${CRAFTIVE_BACKEND_URI:-http://craftive-backend:6060}
+export CRAFTIVE_EXPORTER_URI=${CRAFTIVE_EXPORTER_URI:-http://craftive-exporter:6061}
+CRAFTIVE_DEFAULT_INTERNAL_RESOLVER="$(awk 'BEGIN{ORS=" "} $1=="nameserver" { sub(/%.*$/,"",$2); print ($2 ~ ":")? "["$2"]": $2}' /etc/resolv.conf)"
+export CRAFTIVE_INTERNAL_RESOLVER=${CRAFTIVE_INTERNAL_RESOLVER:-$CRAFTIVE_DEFAULT_INTERNAL_RESOLVER}
+export CRAFTIVE_HTTP_SERVER_MAX_MULTIPART_BODY_SIZE=${CRAFTIVE_HTTP_SERVER_MAX_MULTIPART_BODY_SIZE:-367001600} # Default to 350MiB
 
-envsubst "\$PENPOT_BACKEND_URI,\$PENPOT_EXPORTER_URI,\$PENPOT_HTTP_SERVER_MAX_MULTIPART_BODY_SIZE,\$INCLUDE_PROXIES" \
+envsubst "\$CRAFTIVE_BACKEND_URI,\$CRAFTIVE_EXPORTER_URI,\$CRAFTIVE_HTTP_SERVER_MAX_MULTIPART_BODY_SIZE,\$INCLUDE_PROXIES" \
          < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-envsubst "\$PENPOT_INTERNAL_RESOLVER" \
+envsubst "\$CRAFTIVE_INTERNAL_RESOLVER" \
          < /etc/nginx/overrides.d/resolvers.conf.template > /etc/nginx/overrides.d/resolvers.conf
 
 exec "$@";
