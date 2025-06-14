dafc96380c7e4544c79aa78ec1688cf0e40148d8
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 20:50:04 +0530

    Update build.sh
    Signed-off-by: Ayush2025 <88815689+Ayush2025@users.noreply.github.com>

--- docker/images/build.sh
+++ docker/images/build.sh
@@ -2,31 +2,31 @@
 set -x
 
 DOCKER_CLI_EXPERIMENTAL=enabled
-ORG=${PENPOT_DOCKER_NAMESPACE:-penpotapp};
-PLATFORM=${PENPOT_BUILD_PLATFORM:-linux/amd64};
+ORG=${CRAFTIVE_DOCKER_NAMESPACE:-craftiveapp};
+PLATFORM=${CRAFTIVE_BUILD_PLATFORM:-linux/amd64};
 
-IMAGE=${PENPOT_BUILD_IMAGE:-backend}
-PLATFORM=${PENPOT_BUILD_PLATFORM:-linux/amd64};
-VERSION=${PENPOT_BUILD_VERSION:-latest}
+IMAGE=${CRAFTIVE_BUILD_IMAGE:-backend}
+PLATFORM=${CRAFTIVE_BUILD_PLATFORM:-linux/amd64};
+VERSION=${CRAFTIVE_BUILD_VERSION:-latest}
 
 DOCKER_IMAGE="$ORG/$IMAGE";
 OPTIONS="-t $DOCKER_IMAGE:$VERSION";
 
 IFS=", "
-read -a TAGS <<< $PENPOT_BUILD_TAGS;
+read -a TAGS <<< $CRAFTIVE_BUILD_TAGS;
 
 for element in "${TAGS[@]}"; do
     OPTIONS="$OPTIONS -t $DOCKER_IMAGE:$element";
 done
 
-docker buildx inspect penpot > /dev/null 2>&1;
+docker buildx inspect craftive > /dev/null 2>&1;
 docker run --privileged --rm tonistiigi/binfmt --install all
 
 if [ $? -eq 1 ]; then
-    docker buildx create --name=penpot --use
+    docker buildx create --name=craftive --use
     docker buildx inspect --bootstrap > /dev/null 2>&1;
 else
-    docker buildx use penpot;
+    docker buildx use craftive;
     docker buildx inspect --bootstrap  > /dev/null 2>&1;
 fi
 
